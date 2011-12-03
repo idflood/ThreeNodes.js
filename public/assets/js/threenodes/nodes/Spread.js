984ea343f8db6eea20bf7cd43a7de27924e0a662
David Mignot <david.mig@gmail.com>
2011-12-03 09:08:30 +0100

    add linearspread, little randomspread fix

--- public/assets/js/threenodes/nodes/Spread.js
+++ public/assets/js/threenodes/nodes/Spread.js
@@ -7,7 +7,7 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
   return child;
 };
 define(['jQuery', 'Underscore', 'Backbone', "text!templates/node.tmpl.html", "order!libs/jquery.tmpl.min", "order!libs/jquery.contextMenu", "order!libs/jquery-ui/js/jquery-ui-1.9m6.min", 'order!threenodes/core/NodeFieldRack', 'order!threenodes/utils/Utils'], function($, _, Backbone, _view_node_template) {
-  return ThreeNodes.nodes.types.Spread.RandomSpread = (function() {
+  ThreeNodes.nodes.types.Spread.RandomSpread = (function() {
     __extends(RandomSpread, ThreeNodes.NodeBase);
     function RandomSpread() {
       this.compute = __bind(this.compute, this);
@@ -37,20 +37,68 @@ define(['jQuery', 'Underscore', 'Backbone', "text!templates/node.tmpl.html", "or
       return this.v_out = this.rack.get("out", true);
     };
     RandomSpread.prototype.compute = function() {
-      var i, needs_rebuild, offset, width, _ref;
+      var i, needs_rebuild, _ref;
       needs_rebuild = false;
-      if (this.seed !== this.rack.get("seed").val || this.count !== this.rack.get("count").val || this.width !== this.rack.get("width").val || this.offset !== this.rack.get("offset").val) {
+      if (this.seed !== this.rack.get("seed").val || this.count !== parseInt(this.rack.get("count").get(0)) || this.width !== this.rack.get("width").val || this.offset !== this.rack.get("offset").val) {
         this.seed = this.rack.get("seed").val;
         this.rnd = new ThreeNodes.Utils.Rc4Random(this.seed.toString());
         this.value = [];
-        width = this.rack.get("width").get(0);
-        offset = this.rack.get("offset").get(0);
-        for (i = 0, _ref = this.rack.get("count").get(0); 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
-          this.value[i] = this.rnd.getRandomNumber() * width - width / 2 + offset;
+        this.width = this.rack.get("width").get(0);
+        this.offset = this.rack.get("offset").get(0);
+        this.count = parseInt(this.rack.get("count").val);
+        for (i = 0, _ref = this.count - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+          this.value[i] = this.rnd.getRandomNumber() * this.width - this.width / 2 + this.offset;
         }
       }
       return this.rack.set("out", this.value);
     };
     return RandomSpread;
+  })();
+  return ThreeNodes.nodes.types.Spread.LinearSpread = (function() {
+    __extends(LinearSpread, ThreeNodes.NodeBase);
+    function LinearSpread() {
+      this.compute = __bind(this.compute, this);
+      this.set_fields = __bind(this.set_fields, this);
+      LinearSpread.__super__.constructor.apply(this, arguments);
+    }
+    LinearSpread.prototype.set_fields = function() {
+      LinearSpread.__super__.set_fields.apply(this, arguments);
+      this.auto_evaluate = true;
+      this.value = false;
+      this.count = false;
+      this.width = false;
+      this.phase = false;
+      this.offset = false;
+      this.rack.addFields({
+        inputs: {
+          "count": 1,
+          "width": 1,
+          "phase": 0,
+          "offset": 0
+        },
+        outputs: {
+          "out": 0
+        }
+      });
+      return this.v_out = this.rack.get("out", true);
+    };
+    LinearSpread.prototype.compute = function() {
+      var i, needs_rebuild, res, shift, stepSize, _ref;
+      needs_rebuild = false;
+      this.width = this.rack.get("width").get(0);
+      this.offset = this.rack.get("offset").get(0);
+      this.phase = this.rack.get("phase").get(0);
+      this.count = parseInt(this.rack.get("count").val);
+      this.value = [];
+      stepSize = this.width / this.count;
+      shift = stepSize / 2;
+      for (i = 0, _ref = this.count - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        res = (i * stepSize + shift + this.phase) % this.width;
+        res = this.offset - this.width / 2 + res;
+        this.value[i] = res;
+      }
+      return this.rack.set("out", this.value);
+    };
+    return LinearSpread;
   })();
 });
--- src/coffee/threenodes/nodes/Spread.coffee
+++ src/coffee/threenodes/nodes/Spread.coffee
@@ -32,13 +32,51 @@ define [
   
     compute: =>
       needs_rebuild = false
-      if @seed != @rack.get("seed").val || @count != @rack.get("count").val || @width != @rack.get("width").val || @offset != @rack.get("offset").val
+      if @seed != @rack.get("seed").val || @count != parseInt(@rack.get("count").get(0)) || @width != @rack.get("width").val || @offset != @rack.get("offset").val
         @seed = @rack.get("seed").val
         @rnd = new ThreeNodes.Utils.Rc4Random(@seed.toString())
         
         @value = []
-        width = @rack.get("width").get(0)
-        offset = @rack.get("offset").get(0)
-        for i in [0..@rack.get("count").get(0)]
-          @value[i] = @rnd.getRandomNumber() * width - width / 2 + offset
+        @width = @rack.get("width").get(0)
+        @offset = @rack.get("offset").get(0)
+        @count = parseInt(@rack.get("count").val)
+        for i in [0..@count - 1]
+          @value[i] = @rnd.getRandomNumber() * @width - @width / 2 + @offset
       @rack.set("out", @value)
+  
+  class ThreeNodes.nodes.types.Spread.LinearSpread extends ThreeNodes.NodeBase
+    set_fields: =>
+      super
+      @auto_evaluate = true
+      @value = false
+      @count = false
+      @width = false
+      @phase = false
+      @offset = false
+      @rack.addFields
+        inputs:
+          "count": 1
+          "width" : 1
+          "phase" : 0
+          "offset": 0
+        outputs:
+          "out" : 0
+      
+      @v_out = @rack.get("out", true)
+  
+    compute: =>
+      needs_rebuild = false
+      @width = @rack.get("width").get(0)
+      @offset = @rack.get("offset").get(0)
+      @phase = @rack.get("phase").get(0)
+      @count = parseInt(@rack.get("count").val)
+      @value = []
+      
+      stepSize = @width / @count
+      shift = stepSize / 2
+      for i in [0..@count - 1]
+        res = ( i * stepSize + shift + @phase ) % @width
+        res = @offset - @width / 2 + res
+        @value[i] = res
+      @rack.set("out", @value)
+      
