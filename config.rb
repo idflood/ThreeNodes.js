fdf195ab1da4253ceb14733b0e0a4851d9ef2a5f
David Mignot <david.mig@gmail.com>
2013-09-29 15:52:53 +0200

    move assets out of public folder

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+# require "susy"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/styles"
+sass_dir = "src/styles"
+images_dir = "src/images"
+fonts_dir = "src/fonts"
+javascripts_dir = "assets/scripts/vendors"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# sass_options = {:debug_info => true}
+# output_style = :expanded
+output_style = :expanded
+line_comments = true
